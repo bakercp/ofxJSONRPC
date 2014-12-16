2c06920166a366e0511bbd45a0027e1127d023d5
Christopher Baker <me@christopherbaker.net>
2014-12-16 11:20:28 -0600

    Update addon_config.mk.

--- addon_config.mk
+++ addon_config.mk
@@ -1,6 +1,8 @@
 meta:
 	ADDON_NAME = ofxJSONRPC
-	ADDON_DESCRIPTION = A JSONRPC server for use with Websockets or Post Requests 
+	ADDON_DESCRIPTION = A JSONRPC server for use with Websockets or Post Requests
 	ADDON_AUTHOR = Christopher Baker
 	ADDON_TAGS = "http" "json" "jsonrpc"
 	ADDON_URL = http://github.com/bakercp/ofxJSONRPC
+common:
+	ADDON_DEPENDENCIES = ofxJSON ofxHTTP
